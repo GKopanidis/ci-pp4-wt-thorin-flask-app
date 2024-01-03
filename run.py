a60a5fe422aeedee59d1f28c6544941a539000b6
Georgios Kopanidis <kopanidis@me.com>
2024-01-03 11:20:22 +0000

    Add advanced routing to about section

--- run.py
+++ run.py
@@ -18,6 +18,16 @@ def about():
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
 
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
 
 @app.route("/contact")
 def contact():
